75d95f0444a70ac3a2df486083153161664ab65f
nomore001 <blaze.indy@gmail.com>
2014-07-09 13:42:41 +0700

    init indy02

---
+++ src/fillRegisterProfile/Login.java
@@ -0,0 +1,39 @@
+package fillRegisterProfile;
+
+public class Login {
+	private String username;
+	private String password;
+	private String status;
+	public Login() {
+		
+	}
+
+	public Login(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
